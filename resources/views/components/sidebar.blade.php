7367e53df5b2d4386e34af44d657540c3f871946
SACHITH-KAVISHKA <sachithkavishka12@gmail.com>
2026-06-30 11:43:23 +0530

    fix: Remove "(New)" label from Price List Activity in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -54,7 +54,7 @@
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <span>Price List Activity (New)</span>
+                    <span>Price List Activity</span>
                 </a>
             </div>
         </div>
